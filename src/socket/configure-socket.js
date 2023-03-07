d05bd4e1df0175f077767743534cbcf9ac3aefb8
Fabrizio Dante Pauselli <fabriziopauselli@gmail.com>
2023-03-07 20:20:09 -0300

    cambio el nombre de la variable data a user

--- src/socket/configure-socket.js
+++ src/socket/configure-socket.js
@@ -4,12 +4,13 @@ export default function configureSocket(httpServer) {
   const io = new Server(httpServer);
   io.on('connection', (socket) => {
     console.log('new connection:', socket.id);
-    socket.on('message', (data) => {
-      console.log('message', data);
-      messages.push(data);
+    socket.on('message', (user) => {
+      console.log('message', user);
+      messages.push(user);
       console.log({ messages });
       io.emit('messageLogs', messages);
     });
+
     socket.on('new_user', (data) => {
       console.log('new user:', data);
       socket.emit('messageLogs', messages);
